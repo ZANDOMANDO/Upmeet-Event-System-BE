3d4053f8b55a1dd0594a846b1b6772e2a7b05ebd
Eddy <eduardo.di.garcia@gmail.com>
2022-11-14 20:24:23 -0500

    Initialized be

---
+++ UpmeetBE/dbModels/eventDBContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace UpmeetBE.dbModels
+{
+    public class eventDBContext : DbContext
+    {
+        public eventDBContext(DbContextOptions<DbContext> options) : base(options) { }
+
+        public DbSet<garret> Orders { get; set; }
+    }
+}
---
+++ UpmeetBE/dbModels/favoriteEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UpmeetBE.dbModels
+{
+    public class favoriteEvent
+    {
+        public favoriteEvent()
+        {
+        }
+    }
+}
+
